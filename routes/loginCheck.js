fa2e268c493f8dbd6c313d33c4849faa6aab8c9b
Rohith P <rohithcnss@gmail.com>
2020-04-04 00:05:38 +0530

    raathri 12mani

--- routes/loginCheck.js
+++ routes/loginCheck.js
@@ -1,39 +1,37 @@
-
 var express = require('express');
 var router = express.Router();
 
-
 router.post('/', function(req, res, next) {
-  console.log(req.body);
-  // console.log('hello');
+
+  const number = req.body.voterID;
+  const password = req.body.password;
+  const getUserQuery = {voterID : number};
 
   var MongoClient = require('mongodb').MongoClient;
-  var url = "mongodb://localhost:27017/";
+  var url = "mongodb://localhost:27018/";
   var dbConn = MongoClient.connect(url, function(err, db) {
   if (err) throw err;
   var dbo = db.db("BAVI");
-  // var database = db.db("BAVI");
-  console.log(req.body.aadhaar);
-  // var no = req.body.aadhaar;
-  // console.log(no);
-  // console.log(dbo.collection("voterslist").find({ "name": "Hyzam Ali"}).count().limit(1));
-  var arr = [];
-  dbo.collection("voterslist").find().forEach(function(u) { arr.push(u.aadhaar) });
-  //console.log(dbo.collection("voterslist").find().pretty()); UNCOMMENT THIS LINE TO SEE GIBBERISH GETTING PRINTED
-  console.log(JSON.stringify(arr));
-  if(Array.isArray(req.body.aadhaar))
-  {
-  	console.log('Existing user');
-  }
-  else 
-  {
-  	console.log('User does not exist');
-  }
-  // console.log(no);
-  // console.log('Hello')
-   db.close();
-	// });
-});
+
+  dbo.collection('voterslist').find(getUserQuery).toArray(function(err, result) {
+    if (err) throw err;
+    
+    if(result[0]==null) {
+      console.log('No user found');
+    }else{
+      console.log('User found');
+
+      if(password === result[0].password) {
+        console.log('Correct password');
+        res.render('userHome', {title: 'Registration'});
+      }else {
+        console.log('Wrong Password');
+      }
+
+    }
+  })
+  db.close();
   });
+});
 
 module.exports = router;
